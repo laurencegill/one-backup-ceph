a74056ec483bb0acde26eba2168a25f9b0804943
Laurence Gill <laurencegill@users.noreply.github.com>
2017-12-13 16:35:40 +0000

    add $2 to remove from a different dir

--- removevm.sh
+++ removevm.sh
@@ -4,14 +4,14 @@ set -e
 
 USAGE="`basename $0` <vmname>"
 
-if [ $# -ne 1 ]; then
+if [ $# -lt 1 ]; then
   echo "\nUsage: $USAGE"
   exit 1
 fi
 
 HOST="${1:-onevm}"
+EXPORT_PATH="${2:-/var/lib/bareos/}"
 BACKUP="${HOST}_snap_disk"
-EXPORT_PATH="/var/lib/bareos"
 
 for i in `find ${EXPORT_PATH} -maxdepth 1 -name ${BACKUP}\* -print |tr "\n" " "`; do 
   find $i -delete
